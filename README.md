3eab35b27ebae78f1cd93cd1dfc7c5a24ce9f1e0
alice <58637860+alicealys@users.noreply.github.com>
2025-06-29 00:51:07 +0200

    changes + new binds

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 - binds **WASD** to **movement** [UP, DOWN, RIGHT, LEFT]
 - binds **MOUSE1**/**SPACE** to **interact** [ENTER]
+- binds **MOUSE2** to **cancel** [X]
 - binds **TAB** to **menu** [C]
 
 copy d3d9.dll from **[release](https://github.com/alicealys/undertale-mod/releases)** into the game folder. run the game. ur welcome
--- src/component/patches.cpp
+++ src/component/patches.cpp
@@ -10,35 +10,26 @@ namespace patches
 	{
 		utils::hook::detour wnd_proc_hook;
 
-		std::unordered_map<std::uint32_t, std::uint32_t> binds;		// native to custom
-		std::unordered_map<std::uint32_t, std::uint32_t> binds_rev; // custom to native
+		struct event_t
+		{
+			std::uint32_t target_msg;
+			std::uint32_t target_param;
+			std::uint32_t msg;
+			std::uint32_t param;
+		};
+
+		std::unordered_map<std::uint32_t, std::uint32_t> binds; // native to custom
+		std::vector<event_t> event_detours;
 
 		LRESULT __stdcall wnd_proc_stub(HWND hwnd, UINT msg, WPARAM w_param, LPARAM l_param)
 		{
-			switch (msg)
+			for (const auto& detour : event_detours)
 			{
-			case WM_KEYDOWN:
-			case WM_KEYUP:
-			{
-				const auto iter = binds_rev.find(w_param);
-				if (iter != binds_rev.end())
+				if (msg == detour.target_msg && (detour.target_param == 0 || detour.target_param == w_param))
 				{
-					w_param = iter->second;
+					msg = detour.msg;
+					w_param = detour.param;
 				}
-			}
-			break;
-			case WM_LBUTTONDOWN:
-			{
-				msg = WM_KEYDOWN;
-				w_param = VK_RETURN;
-			}
-			break;
-			case WM_LBUTTONUP:
-			{
-				msg = WM_KEYUP;
-				w_param = VK_RETURN;
-			}
-			break;
 			}
 
 			return wnd_proc_hook.invoke_pascal<LRESULT>(hwnd, msg, w_param, l_param);
@@ -57,10 +48,29 @@ namespace patches
 			return result;
 		}
 
+		void add_event_detour(const std::uint32_t target_msg, const std::uint32_t new_msg, const std::uint32_t target_param, std::uint32_t param)
+		{
+			event_t ev{};
+			ev.target_msg = target_msg;
+			ev.target_param = target_param;
+
+			ev.msg = new_msg;
+			ev.param = param;
+
+			event_detours.emplace_back(ev);
+		}
+
 		void add_keybind(const std::uint32_t custom, const std::uint32_t native)
 		{
+			add_event_detour(WM_KEYUP, WM_KEYUP, custom, native);
+			add_event_detour(WM_KEYDOWN, WM_KEYDOWN, custom, native);
+
 			binds.insert(std::make_pair(native, custom));
-			binds_rev.insert(std::make_pair(custom, native));
+		}
+
+		void add_mouse_bind(const std::uint32_t custom, const std::uint32_t native, const bool up)
+		{
+			add_event_detour(custom, WM_KEYDOWN + up, 0, native);
 		}
 	}
 
@@ -78,8 +88,15 @@ namespace patches
 			add_keybind('S', VK_DOWN);
 			add_keybind('A', VK_LEFT);
 			add_keybind('D', VK_RIGHT);
+
 			add_keybind(VK_SPACE, VK_RETURN);
 			add_keybind(VK_TAB, 'C');
+
+			add_mouse_bind(WM_LBUTTONDOWN, VK_RETURN, 0);
+			add_mouse_bind(WM_LBUTTONUP, VK_RETURN, 1);
+
+			add_mouse_bind(WM_RBUTTONDOWN, 'X', 0);
+			add_mouse_bind(WM_RBUTTONUP, 'X', 1);
 
 			utils::hook::nop(0x52970C, 6);
 			utils::hook::call(0x52970C, get_async_get_state);
